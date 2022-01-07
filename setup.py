a8f6297f9ad1e4c218fcbdfac31a73a6b99bb25d
durgeshrawat <66310896+durgeshrawat@users.noreply.github.com>
2022-01-07 20:48:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'KEEpydb',         # How you named your package folder (MyLib)
   packages = ['KEEpydb'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Database for Python',   # Give a short description about your library
   long_description='for More information and example code for KEEpydb visit : https://keepydb.wordpress.com',#full description
