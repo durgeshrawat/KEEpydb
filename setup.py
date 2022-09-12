ec3caf79569c8f03e83ee3378e139450bd7f2819
durgeshrawat <66310896+durgeshrawat@users.noreply.github.com>
2022-09-12 11:43:41 +0530

    2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'KEEpydb',         # How you named your package folder (MyLib)
   packages = ['KEEpydb'],   # Chose the same as "name"
-  version = '2.1',      # Start with a small number and increase it with every change you make
+  version = '2.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Offline and Realtime Database for Python',   # Give a short description about your library
   long_description='for More information and example code for KEEpydb visit : https://keepydb.wordpress.com',#full description
